3b974996240236693ed166992eedb54da78a760e
george-of-croton <armstronggeorge10@gmail.com>
2016-10-10 17:55:31 +1100

    add token auth

--- controllers/message.js
+++ controllers/message.js
@@ -1,11 +1,11 @@
-var Message = require('../models.message');
+var Message = require('../models/message');
 
 module.exports = {
 	get: function (req, res) {
 	Message.find({}).exec(function(err, result){
 		res.send(result);
 		})
-	}
+	},
 	post: function(req, res){
 	console.log(req.body);
 
